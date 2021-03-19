87c7e35853dae07e291ba6e196c8767eb5d53fb7
James Galantino <james_galantino@aol.com>
2021-03-18 22:27:25 -0400

    Added a contact component and routed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import AboutMe from './components/AboutMe';
+import Contact from './components/Contact';
 import MyProjects from './components/MyProjects';
-// import Project from './components/Project';
 import Footer from './components/Footer';
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
         <Header />
         <Route exact path='/' component={Home} />
         <Route exact path='/aboutme' component={AboutMe} />
+        <Route exact path='/contact' component={Contact} />
         <Route exact path='/projects' component={MyProjects} />
         <Footer />
       </div>
