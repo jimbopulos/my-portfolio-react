53328c22a90e38605c80b0a245841b9e96287e96
James Galantino <james_galantino@aol.com>
2021-03-19 18:07:31 -0400

    Relocated images dir

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function Project(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div className='card'>
+        <div className='img-container'>
+          <img src={props.image} alt={props.title} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Project;
