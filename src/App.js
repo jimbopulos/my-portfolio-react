f140eda0b3de3ab9e7eb749d0629c8bb1b8b55cf
James Galantino <james_galantino@aol.com>
2021-03-18 21:21:12 -0400

    Imported router components to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import AboutMe from './components/AboutMe';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <div>
+        <Header />
+        <Router exact path='/' component={Home} />
         <h1>Here's my Portfolio</h1>
       </div>
     </Router>
