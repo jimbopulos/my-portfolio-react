60c7b520283fed84a9c839c22aa8684f70c5a064
James Galantino <james_galantino@aol.com>
2021-04-01 11:50:27 -0400

    Styled navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,51 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
   const location = useLocation();
 
   return (
-    <ul className='nav nav-tabs justify-content-end'>
-      <li className='nav-item'>
+    <ul className="nav nav-tabs justify-content-end">
+      <li className="nav-item">
         <Link
-          to='/'
-          className={location.pathname === '/' ? 'nav-link active' : 'nav-link'}
+          to="/"
+          className={
+            location.pathname === "/" ? "nav-link port-nav active" : "nav-link"
+          }
         >
           Home
         </Link>
       </li>
-      <li className='nav-item'>
+      <li className="nav-item">
         <Link
-          to='/aboutme'
+          to="/aboutme"
           className={
-            location.pathname === '/aboutme' ? 'nav-link active' : 'nav-link'
+            location.pathname === "/aboutme"
+              ? "nav-link port-nav active"
+              : "nav-link"
           }
         >
           About
         </Link>
       </li>
-      <li className='nav-item'>
+      <li className="nav-item">
         <Link
-          to='/contact'
+          to="/contact"
           className={
-            location.pathname === '/contact' ? 'nav-link active' : 'nav-link'
+            location.pathname === "/contact"
+              ? "nav-link port-nav active"
+              : "nav-link"
           }
         >
           Contact
         </Link>
       </li>
-      <li className='nav-item'>
+      <li className="nav-item">
         <Link
-          to='/projects'
+          to="/projects"
           className={
-            location.pathname === '/projects' ? 'nav-link active' : 'nav-link'
+            location.pathname === "/projects"
+              ? "nav-link port-nav active"
+              : "nav-link"
           }
         >
           Projects
